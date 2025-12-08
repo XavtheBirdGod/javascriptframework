9511dd48e9cfa8f4a5550bee4a09d5ee482df735
XavtheBirdGod <xanthe.vannestedeloose@gmail.com>
2025-12-08 10:38:59 +0100

    vue

---
+++ vue2025/trello-vue/src/components/TaskCard.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    default: ""
+  }
+})
+</script>
+<template>
+  <article class="rounded-lg bg-gray-800 p-4 shadow-md border border-gray700">
+    <h3 class="font-semibold text-sm text-white">
+      {{ props.title }}
+    </h3>
+    <p v-if="props.description" class="text-xs text-gray-300 mt-1 leadingrelaxed">
+      {{ props.description }}
+    </p>
+  </article>
+</template>
